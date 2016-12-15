ee29c9436201976354f5ded7d3bdaf98e1c32326
sharathnb10 <sharathnb10@outlook.com>
2016-12-15 23:33:44 +0530

    the gulp file contains to code to exccute code on other project

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,10 @@ gulp.task('html', function() {
     return gulp.src('src/index.html')
         .pipe(gulp.dest('desc'));
 });
+gulp.task('projecthtml', function() {
+    return gulp.src('../project2/src/index.html')
+        .pipe(gulp.dest('../project2/desc'));
+});
 
 //task used to concate the javascript and uglify it
 gulp.task('build-js', function() {
